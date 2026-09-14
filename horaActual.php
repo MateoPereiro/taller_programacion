793723f36934217c7622d13d9e9b248e2b2d5a9e
MateoPereiro <mateopereirosc@gmail.com>
2026-09-14 09:52:03 -0300

    2
    2

---
+++ horaActual.php
@@ -0,0 +1,13 @@
+<?php
+
+date_default_timezone_set("America/El_salvador");
+
+$fecha_us=date("l d F Y");
+$fecha_es=date("d-m-Y");
+
+$hora_12=date("h i a");
+$hora_21=date("H:i");
+
+$fecha_completa=date("l d F Y H:i A");
+echo $fecha_completa;
+
